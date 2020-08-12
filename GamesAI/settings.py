7777f7a8b37cbdad0719877fe6bca2a054294c64
Santi <Santiagocastellano01@gmail.com>
2020-08-12 19:09:50 -0300

    config heroku

--- GamesAI/settings.py
+++ GamesAI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 
 from pathlib import Path
 
@@ -128,3 +129,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GamesAI.wsgi --log-file -
