32e48dcb355e5d8f3e0bcd38dcddf223b568677c
Santi <Santiagocastellano01@gmail.com>
2020-08-13 19:09:54 -0300

    juega tateti

--- TaTeTi/views.py
+++ TaTeTi/views.py
@@ -8,14 +8,24 @@ def index(request):
     return render(request,'TaTeTi/Index.html')
 
 def play_ai(request):
-    board= [0,0,0],[0,0,0],[0,0,0]
-    row,col = play_IA(board)
-    board[row][col] = 1
-    
-    return JsonResponse(
-        {
-            "row":row,
-            "col":col
-        }
-    )
+    if request.GET and request.is_ajax():
+        data = dict(request.GET)
+        row_one = list(map(int,  data["board[0][]"]))
+        row_two = list(map(int,  data["board[1][]"]))
+        row_three= list(map(int, data["board[2][]"]))
+        board= row_one , row_two , row_three
+        if 0 not in (row_one+row_two+row_three):
+            return JsonResponse(
+                {
+                    "row":-1,
+                    "col":-1
+                }
+            )
+        row,col = play_IA(board)
+        return JsonResponse(
+            {
+                "row":row,
+                "col":col
+            }
+        )
 
--- static/TaTeTi/js/Index.js
+++ static/TaTeTi/js/Index.js
@@ -19,20 +19,60 @@ function what_show(board) {
 }
 $(what_show(true))
 
+
 function play_ai(){
+    let board = []
+    $("#board tbody tr").each(function (){
+        let row = [];
+        $(this).find("td").each(function (){
+            if ($(this).hasClass("ai")){
+                row.push(1) 
+            }else if ($(this).hasClass("player")){
+                row.push(-1) 
+
+            }
+            else{
+                row.push(0) 
+            }
+        })
+        board.push(row);
+    });
+    
     $.ajax({
         type: "GET",
         url:"/tateti/play_ai/",
         dataType:'json',
-        data: {},
+        data: {
+            board
+        },
         success:function(data){
-            console.log(data);
+            $("#board tbody tr").each(function (){
+                if ($(this).data("row") == data.row) {
+                    $(this).find("td").each(function (){
+                        if ($(this).data("col") == data.col){
+                            $(this).addClass("ai");
+                        }
+                    })
+                } 
+                
+            });
         },
         error: function(error){
             console.log(error)
         },
         complete:function(){
             what_show(true);
+        }
+    });
+}
+
+function play_again(){
+    $("#board tbody tr td").each(function () {
+        if ($(this).hasClass("player")) {
+            $(this).removeClass("player")
+        }
+        if ($(this).hasClass("ai")) {
+            $(this).removeClass("ai")
         }
     });
 }
